3e13543ad91333c3e6f89833d18ecaf9fb5b9200
Igor Zarvanskyi <iclutcher@gmail.com>
2026-03-09 12:40:48 +0100

    feat(configuration): update setup script to not duplicate in bashrc on additional runs

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,15 +1,23 @@
 cp -rf dotfiles/* ~/.config/
 
-# Inject bash configuration on terminal open
-cat <<EOT >> ~/.bashrc
+START_MARKER="# BEGIN awesome-wm-config"
+END_MARKER="# END awesome-wm-config"
+BASH_CONFIG=$(cat <<EOT
 
-
-# Source all .bash_* files from the folder
-if [ -d "$HOME/.config/bash" ] && [ "$(ls -A $HOME/.config/bash)" ]; then
-    for file in "$HOME/.config/bash/.bash_"*; do
-        if [ -f "$file" ]; then
-            source "$file"
+$START_MARKER
+if [ -d "\$HOME/.config/bash" ] && [ "\$(ls -A \$HOME/.config/bash)" ]; then
+    for file in "\$HOME/.config/bash/.bash_"*; do
+        if [ -f "\$file" ]; then
+            source "\$file"
         fi
     done
 fi
+$END_MARKER
 EOT
+)
+
+if grep -qF "$START_MARKER" ~/.bashrc; then
+    sed -i "/$START_MARKER/,/$END_MARKER/d" ~/.bashrc
+fi
+
+echo "$BASH_CONFIG" >> ~/.bashrc
